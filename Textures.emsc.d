332a544e3823797eaa0fc031bafbb5e63eca6a52
NicksterX8 <91515059+NicksterX8@users.noreply.github.com>
2022-07-28 12:36:13 -0500

    Delete Textures.emsc.d

--- Textures.emsc.d
+++
@@ -1,9 +0,0 @@
-Textures.emsc.o: Textures.cpp Textures.hpp \
-  /usr/local/Cellar/sdl2_image/2.0.5/include/SDL2/SDL_image.h \
-  SDL_FontCache/SDL_FontCache.h
-
-Textures.hpp:
-
-/usr/local/Cellar/sdl2_image/2.0.5/include/SDL2/SDL_image.h:
-
-SDL_FontCache/SDL_FontCache.h:
