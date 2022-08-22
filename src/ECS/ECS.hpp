9695d5371f54fc08032c6f2b7a1c0ca0af32b4dc
Nick Wilson <nickjwilson6@gmail.com>
2022-08-21 19:05:49 -0500

    moved to cmake

---
+++ src/ECS/ECS.hpp
@@ -0,0 +1,31 @@
+#ifndef ECS_INCLUDED
+#define ECS_INCLUDED
+
+#include <SDL2/SDL.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <bitset>
+#include <functional>
+#include <vector>
+
+#include "../constants.hpp"
+#include "../NC/cpp-vectors.hpp"
+#include "../Textures.hpp"
+#include "../Log.hpp"
+
+#include "Entity.hpp"
+#include "Component.hpp"
+#include "SystemsConstants.hpp"
+#include "EntityManager.hpp"
+#include "EntitySystemInterface.hpp"
+#include "ComponentPool.hpp"
+#include "../../ComponentMetadata/component.hpp"
+
+
+namespace ECS {
+
+
+
+}
+
+#endif
---
+++ src/GUI.cpp
@@ -0,0 +1,17 @@
+#include "GUI.hpp"
+
+/*
+
+void Draw::itemStack(SDL_Renderer* renderer, float scale, ItemStack item, SDL_Rect* destination) {
+    // icon
+    SDL_RenderCopy(renderer, ItemData[item.item].icon, NULL, destination);
+    // quantity count
+    // dont draw item count over items that can only ever have one count,
+    // its pointless
+    if (ItemData[item.item].stackSize != 1 && item.quantity != INFINITE_ITEM_QUANTITY) {
+        FC_DrawScale(FreeSans, renderer, destination->x + 3, destination->y - 4, FC_MakeScale(scale/2.0f,scale/2.0f),
+        "%d", item.quantity);
+    }
+}
+
+*/
