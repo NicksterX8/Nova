d3df15345891ce4788164ddd80ad4ff07e109ffb
Melanie Wilson <melaniewilson@Melanies-iMac.local>
2022-09-21 22:28:33 -0500

    bug fix
    fix

--- macos/make-app.sh
+++ macos/make-app.sh
@@ -5,7 +5,6 @@ cp macos/info.plist $MAC_APP_PATH/Contents/info.plist
 mkdir -p $MAC_APP_PATH/Contents/MacOS
 cp build/faketorio $MAC_APP_PATH/Contents/MacOS/exe
 mkdir -p $MAC_APP_PATH/Contents/Resources
-cp -r build/resources $MAC_APP_PATH/Contents/Resources
+cp -r build/resources $MAC_APP_PATH/Contents/
 cp macos/favicon.icns $MAC_APP_PATH/Contents/Resources/favicon.icns
 cp macos/PkgInfo $MAC_APP_PATH/Contents/Resources/PkgInfo
-cp -r $MAC_APP_PATH /Users/nick/Desktop/
