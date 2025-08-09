317b6b7e00ab22dffe606d018f423c2e9096ee18
Nick Wilson <nickjwilson6@gmail.com>
2025-08-09 03:31:16 -0500

    entity manager tests

--- tests/src/ecs/components.cpp
+++ tests/src/ecs/components.cpp
@@ -22,6 +22,15 @@ BEGIN_COMPONENT(Health)
     float health;
 END_COMPONENT(Health)
 
+class EntityManagerTest : public testing::Test {
+protected:
+    EntityManager manager;
+    EntityManagerTest() {
+        static constexpr auto info = getComponentInfoList<Position, Health>();
+        manager.init(ArrayRef(info), 0);
+    }
+};
+
 EntityManager* makeEntityManager() {
     EntityManager* m = new EntityManager();
     static constexpr auto info = getComponentInfoList<Position, Health>();
@@ -29,23 +38,34 @@ EntityManager* makeEntityManager() {
     return m;
 }
 
-TEST(EcsComponents, GetComponent) {
-    EntityManager& manager = *makeEntityManager();
-    
-    Entity entity = manager.newEntity(-1);
-    EXPECT_TRUE(manager.entityExists(entity)) << "Failed to create an entity!";
+TEST_F(EntityManagerTest, AddComponent) {
+    Entity entity = this->manager.newEntity(-1);
+    EXPECT_TRUE(this->manager.entityExists(entity)) << "Failed to create an entity!";
 
-    EXPECT_FALSE(manager.entityHas<Position>(entity));
+    EXPECT_FALSE(this->manager.entityHas<Position>(entity));
 
-    Position* nullPos = manager.getComponent<Position>(entity);
+    Position* nullPos = this->manager.getComponent<Position>(entity);
     EXPECT_EQ(nullPos, nullptr);
 
     Position posValue = {5, 10};
-    EXPECT_TRUE(manager.addComponent<Position>(entity, posValue));
-    Position* pos = manager.getComponent<Position>(entity);
+    EXPECT_TRUE(this->manager.addComponent<Position>(entity, posValue));
+    Position* pos = this->manager.getComponent<Position>(entity);
     EXPECT_NE(pos, nullptr);
 
     EXPECT_EQ(*pos, posValue);
 
-    EXPECT_TRUE(manager.entityHas<Position>(entity));
+    EXPECT_TRUE(this->manager.entityHas<Position>(entity));
+
+    this->manager.removeComponent<Position>(entity);
+    EXPECT_FALSE(this->manager.entityHas<Position>(entity));
+    EXPECT_FALSE(this->manager.getComponent<Position>(entity));
+    this->manager.deleteEntity(entity);
+}
+
+TEST_F(EntityManagerTest, UsingDeletedEntity) {
+    Entity entity = this->manager.newEntity(-1);
+    this->manager.addComponent<Position>(entity, {1, 2});
+    this->manager.deleteEntity(entity);
+    EXPECT_FALSE(this->manager.getComponent<Position>(entity));
+    EXPECT_FALSE(this->manager.entityExists(entity));
 }
